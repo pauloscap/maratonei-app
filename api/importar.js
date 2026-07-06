b3ae5819cdb9040216ee41a0d54e4dd45fa598f7
pauloscap <paulor.garcia7@gmail.com>
2026-07-06 16:07:15 -0300

    Update importar.js

--- api/importar.js
+++ api/importar.js
@@ -1,15 +1,14 @@
 import { createClient } from '@supabase/supabase-js'
 
-// COLA A URL E CHAVE REAL AQUI - SÓ PRA TESTE
 const supabase = createClient(
-  'https://pnwzbnmvpzipnlwrewjf.supabase.co', 
-  'sb_secret_cUbmP...eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBud3pibm12cHppcG5sd3Jld2pmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4MzM0NDEyNywiZXhwIjoyMDk4OTIwMTI3fQ.Vcsj8Gu82UV56Vre2a4CshR5U4GsbV3euKzyAUcOfdU...'
+  'https://pnwzbnmvpzipnlwrewjf.supabase.co',
+  'sb_secret_cUbmPT5VAJFppkLezqyjKA_LLSrXo8U'
 )
 
 export default async function handler(req, res) {
   const teste = {
     id_tmdb: 1396,
-    tipo: 'serie', 
+    tipo: 'serie',
     titulo: 'Breaking Bad',
     ano: 2008
   }
@@ -21,9 +20,9 @@ export default async function handler(req, res) {
       ok: false,
       erro_supabase: error.message,
       codigo: error.code,
-      msg: "HARDCODE TAMBÉM FALHOU"
+      msg: "AINDA DEU RUIM"
     })
   }
 
-  res.status(200).json({ ok: true, inseriu: data, msg: "HARDCODE FUNCIONOU!" })
+  res.status(200).json({ ok: true, inseriu: data, msg: "FUNCIONOU CARALHO!" })
 }
